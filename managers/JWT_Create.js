2376fe4efe244d934c14b051bf68d605a7814188
Daniel Diner <danrnr12@gmail.com>
2018-06-01 20:20:06 -0500

    Returned JWT_Create

---
+++ managers/JWT_Create.js
@@ -0,0 +1,35 @@
+import jwt from 'jsonwebtoken'
+// var key =require('./confing')
+import {key} from '../config/config'
+
+
+const cabecera={
+    type:'JWT',
+    alg:"HS256"
+}
+
+
+function verificarToken(token,key){
+    jwt.verify(token,key,
+    function(err,data){
+        if(err){
+            console.log("error")
+            return(403)
+        }
+        else{
+            console.log("-----------")
+            console.log(data)
+            return(data)
+        }
+    }) 
+
+
+}
+
+
+// var res= verificar(token,key)
+//let res= verificarToken(token,key)
+
+export {
+    verificarToken
+} 
