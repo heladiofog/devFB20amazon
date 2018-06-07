0d7106d1abb6390e0c6619b80fda5569c3333f8c
helladyo <heladiofog@gmail.com>
2018-06-07 10:12:16 -0500

    Sharing test associations  with Abel.

---
+++ models/product_test.js
@@ -0,0 +1,22 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  var Product = sequelize.define('Product', {
+    id: {
+      type: DataTypes.UUID,
+      primaryKey: true,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false,
+      autoIncrement: false,
+    },
+    name: DataTypes.STRING,
+    desc: DataTypes.STRING,
+    price: DataTypes.NUMERIC
+  }, {});
+  Product.associate = function(models) {
+    Product.belongsToMany(models.Category, 
+      {
+        through: 'ProductCategory'
+    });
+  };
+  return Product;
+};
