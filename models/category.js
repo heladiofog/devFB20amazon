693b6f0dea367995139d9959ac6284e4b1337a41
Salazar <antonio.salazar.emrsn@gmail.com>
2018-06-06 19:33:46 -0500

    adding Heladio's changes

--- models/category.js
+++ models/category.js
@@ -1,20 +1,19 @@
-'use strict';
 module.exports = (sequelize, DataTypes) => {
-  const Category = sequelize.define("Category", {
-      name: {
-          type: DataTypes.STRING,
-          unique: true,
-          allowNull: false
-      },
-      description:{
-          type: DataTypes.STRING,
-          allowNull: false
-      }
-  })
+    const Category = sequelize.define("Category", {
+        name: {
+            type: DataTypes.STRING,
+            unique: true,
+            allowNull: false
+        },
+        description:{
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+    })
 
-  Category.associate = function(models){
+    Category.associate = function(models){
 
-  };
-  
-  return Category;
+    };
+    
+    return Category;
 };
--- models/user.js
+++ models/user.js
@@ -1,90 +1,73 @@
-'use strict';
 import bcrypt from 'bcrypt-nodejs'
 module.exports = (sequelize, DataTypes) => {
-const User = sequelize.define('User', {
-    email: {
-      type: DataTypes.STRING,
-      unique: true,
-      validate:{
-          isEmail: true,
-          notEmpty: true
-      }
-    },
-    nombre: {
-        type: DataTypes.STRING,
-        validate: {
-            notEmpty: true
-        }
-    },
-    
-    
-    password:{
-        type: DataTypes.STRING,
-        validate: {
-            notEmpty: true
-        }
-    },
-
-    gender:{
-        type: DataTypes.STRING,
-        validate: {
-            allowNull: true
-        }
-    },
-    
-    birdthDate:{
-        type: DataTypes.STRING,
-        validate:{
-            isDate: true,
-            allowNull: true
-        }
-    },
-    //Sirve para ver el estado del usuario (borrado logico)
-    status:{ 
-        type: DataTypes.BOOLEAN,
-        validate:{
-           allowNull: false,
-           defaultValue: true 
-        } 
+    const User = sequelize.define('User', {
+        email: {
+            type: DataTypes.STRING,
+            unique: true,
+            validate:{
+                isEmail: true,
+            },
+            allowNull: false
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
         
-    }
-
-  })
-
-//   genero:{}
-
-  User.beforeCreate((user)=>{
-    return crypt(user.password) 
-       .then(success => {user.password = success})
-       .catch(err => {
-           if (err) 
-        console.log(err)
-       })
         
-   })
-   
-   let crypt = (password) => {
-       return new Promise ((resolve,reject)=>{
-        bcrypt.genSalt(10,(err,salt)=>{
-            if(err) reject(err)
-            bcrypt.hash(password,salt,null,(err,hash) => {
+        password:{
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        
+        gender:{
+            type: DataTypes.STRING,
+            allowNull: true
+        },
+        
+        birthDate:{
+            type: DataTypes.DATE,
+            allowNull: true
+        },
+        //Sirve para ver el estado del usuario (borrado logico)
+        status:{ 
+            type: DataTypes.BOOLEAN,
+            validate:{
+               defaultValue: true 
+            } 
+        }
+        
+    })
+    
+    //   genero:{}
+    
+    User.beforeCreate((user)=>{
+        return crypt(user.password) 
+        .then(success => {user.password = success})
+        .catch(err => {
+            if (err) 
+            console.log(err)
+        })
+        
+    })
+    
+    let crypt = (password) => {
+        return new Promise ((resolve,reject)=>{
+            bcrypt.genSalt(10,(err,salt)=>{
                 if(err) reject(err)
-                resolve(hash)
-                
+                bcrypt.hash(password,salt,null,(err,hash) => {
+                    if(err) reject(err)
+                    resolve(hash)
+                    
+                });
             });
         });
-    });
-   }
-  User.associate = function(models) {
-    // associations can be defined here
-  };
-  return User;
+    }
+    User.associate = function(models) {
+        // associations can be defined here
+    };
+    return User;
 }
 
-// module.exports = (sequelize, DataTypes) => {
-//   var User = sequelize.define('User', {}, {});
-//   User.associate = function(models) {
-//     // associations can be defined here
-//   };
-//   return User;
-// };
+
+
