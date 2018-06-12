69266a449798f7d987a6b56315544e228a782fa5
Salazar <antonio.salazar.emrsn@gmail.com>
2018-06-11 21:00:23 -0500

    started with the category controller, created the endpoint to create a category, also added documentation on the createUser endpoint

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ import db from './models';
 
 
 const app = express()
-const port = process.env.PORT || 8080
+const port = process.env.PORT || 3000
 
 app.use(parser.urlencoded({extended : true}))
 app.use(parser.json())
